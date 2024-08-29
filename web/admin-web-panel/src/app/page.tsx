c92aa480992844386abc964447f9774d8da54c48
AlienCake <42356750+Aliencake@users.noreply.github.com>
2024-08-29 19:53:29 +0300

    MIN-10: configure-nextjs-application-with-nextauth (#1)
    * feat: add prettier
    
    * feat: add shadcn
    
    * feat: configure nextAuth

--- web/admin-web-panel/src/app/page.tsx
+++ web/admin-web-panel/src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/admin/login");
+    },
+  });
+
+  if (status === "loading") {
+    return <div>Loading</div>;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        Hello World!
-      </h1>
+      <h1>Hello World!</h1>
     </main>
   );
 }
