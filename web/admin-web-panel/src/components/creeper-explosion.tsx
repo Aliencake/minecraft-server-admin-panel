9a5a13ad3b6563f15f485c51f6640c76624baaa2
Aliencake <voloshko30@gmail.com>
2024-08-31 11:17:02 +0300

    feat: add creeper explosion animation on successful login

---
+++ web/admin-web-panel/src/components/creeper-explosion.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import * as React from "react";
+
+interface ExplosionGifProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export default function ExplosionGif(props: ExplosionGifProps) {
+    const {className} = props
+
+    return (
+        <div className={className}>
+            <Image src="explosion-minecraft.gif" alt="explosion gif" fill={true} unoptimized={true} />
+        </div>
+    );
+}
--- web/admin-web-panel/src/components/creeper-scene.tsx
+++ web/admin-web-panel/src/components/creeper-scene.tsx
@@ -7,15 +7,15 @@ function Model({ url }: { url: string }) {
     return <primitive object={scene} scale={1} />;
 }
 
-export default function Scene() {
+export default function CreeperScene() {
     return (
         <Canvas
             style={{ height: '30vh', backgroundColor: 'inherit'}}
-            camera={{ position: [2, 2, 40], fov: 50 }}
+            camera={{ position: [2, 15, 40], fov: 50 }}
         >
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 5]} intensity={1} />
-            <OrbitControls autoRotate autoRotateSpeed={5} enableZoom={false} />
+            <OrbitControls autoRotate autoRotateSpeed={4} enableZoom={false} />
             <Model url="/minecraft_creeper.glb" />
         </Canvas>
     );
